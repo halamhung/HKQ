9bb42851b3c25093a09f8cd9b5f6b1d2b8a4b0e9
halamhung <hungdeptrai111@gmail.com>
2023-05-14 16:29:19 +0700

    hoan chinh 14_05_2023

---
+++ HQKTravel/HQKTravel/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HQKTravel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+          
+
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult Travel()
+        {
+            return View();
+        }
+        
+        public ActionResult Contact()
+        {
+  
+
+            return View();
+        }
+    }
+}
